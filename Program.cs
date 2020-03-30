713da89f7f4b8efb8d917f17696cadff96693830
smx_2019_08_22 <itmarko33@gmail.com>
2020-03-30 15:03:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRepositoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseRepository<Customer> br = new BaseRepository<Customer>(new GRPDemoEntities());
+            Customer customer = new Customer();
+            customer.id = 1;
+            customer.name = "Marko";
+            br.Insert(customer);
+            br.Save();
+        }
+    }
+}
