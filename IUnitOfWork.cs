57eb3acaf00a319e8de0788e9e2cb01b782f506a
smx_2019_08_22 <itmarko33@gmail.com>
2020-03-30 19:55:17 +0200

    Added IUnitOfWork and UnitOfWork to project.

---
+++ IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRepositoryPattern
+{
+    interface IUnitOfWork
+    {
+        IRepository<Customer> Customers { get; }
+        IRepository<Order> Orders { get; }
+        void commit();
+    }
+}
---
+++ UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRepositoryPattern
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        GRPDemoEntities _grpde;
+        IRepository<Customer> _Customers;
+        IRepository<Order> _Orders;
+
+        public UnitOfWork(GRPDemoEntities grpde)
+        {
+            _grpde = grpde;
+        }
+
+        public IRepository<Customer> Customers 
+        {
+            get
+            {
+                return _Customers ??
+                       (_Customers = new BaseRepository<Customer>(_grpde));
+            }
+        }
+
+        public IRepository<Order> Orders 
+        {
+            get 
+            {
+                return _Orders ??
+                      (_Orders = new BaseRepository<Order>(_grpde));
+                    }
+        }
+
+        public void commit()
+        {
+            _grpde.SaveChanges();
+        }
+    }
+}
